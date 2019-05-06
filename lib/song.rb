b12f423a877a4ae6b29b9e184a0b2fc2a953f04c
Michelle Moon <michellemoon236@gmail.com>
2019-05-06 20:38:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,7 @@ class Song
     artist_name = filename.split(" - ")[0]
     song = Song.new(song_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
+    song.artist.add_song(song)
     song
   end
   
